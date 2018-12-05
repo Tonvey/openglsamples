831587529a334977824a5b4472bcb98fcfdeeb3b
liangboyan <liangboyanzaz@163.com>
2018-12-05 11:09:32 +0800

    glfw test

--- 13_glfw_hello/glfw_hello.cpp
+++ 13_glfw_hello/glfw_hello.cpp
@@ -1,20 +1,18 @@
 #include "MyStudyHeaders.h"
-#include <math.h>
+#include <cmath>
 #include <cstdlib>
 #include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
 namespace tonvey {
-
     void display(GLFWwindow *window)
     {
-        static int frameNo = 1;
         glClearColor(0,0,0.4,1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
     }
     void (*init_func)(int argc , char **argv)=nullptr;
     void (*display_func)(GLFWwindow *)=display;
     void (*key_func)(GLFWwindow *,int,int,int,int)=nullptr;
     void (*error_func)(int error , const char *description)=nullptr;
-
 }
