f1ef26cd09647180398547081720e78be1795e48
Tonvey <liangboyanzaz@163.com>
2019-01-01 22:38:28 +0800

    adjust cmake file and header file to exclude uneccessary files

--- common/MyStudyHeaders.h
+++ common/MyStudyHeaders.h
@@ -8,10 +8,7 @@
 #   include <GLUT/glut.h>
 #else
 #define GL_GLEXT_PROTOTYPES
-#   include <GL/gl.h>
 #   include <GL/glut.h>
-#   include <GL/glu.h>
-#   include <GL/glext.h>
 #endif
 
 #define MY_LOG_DEBUG(...) do{printf(__VA_ARGS__);fflush(stdout);}while(0)
