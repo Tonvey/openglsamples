32066f1f73e8c473a63920fce63f1860d4cd340a
liangboyan <liangboyanzaz@163.com>
2019-01-02 00:03:19 +0800

    17 ebo

---
+++ 17_glsl_ebo/main.cpp
@@ -0,0 +1,105 @@
+#include "ApplicationCoreProfile.h"
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include "FileUtil.h"
+using namespace std;
+#define VERTEX_FILE_NAME "shader.vert"
+#define FRAG_FILE_NAME "shader.frag"
+
+class Application: public ApplicationCoreProfile
+{
+    private:
+        GLuint myProgramId;
+        GLuint vertexPosition;
+        GLuint vbo;
+        GLuint vao;
+    public:
+
+        Application(int argc , char **argv)
+            :ApplicationCoreProfile(argc,argv)
+        {
+        }
+        ~Application()
+        {
+            if(glIsBuffer(vbo)==GL_TRUE)
+            {
+                cout<<"Delete vbo buffer"<<endl;
+                glDeleteBuffers(1,&vbo);
+            }
+            if(glIsVertexArray(vao)==GL_TRUE)
+            {
+                cout<<"Delete vao buffer"<<endl;
+                glDeleteVertexArrays(1,&vao);
+            }
+            if(glIsProgram(myProgramId)==GL_TRUE)
+            {
+                cout<<"Delete program"<<endl;
+                glDeleteProgram(myProgramId);
+            }
+        }
+        void init()override
+        {
+            ApplicationCoreProfile::init();
+            //加载shader
+            myProgramId = loadShader(
+                    FileUtil::getFileDirName(__FILE__)+FileUtil::pathChar+VERTEX_FILE_NAME,
+                    FileUtil::getFileDirName(__FILE__)+FileUtil::pathChar+FRAG_FILE_NAME
+                    );
+
+            vertexPosition= 
+                glGetAttribLocation(myProgramId,"vertexPosition");
+            cout<<vertexPosition<<endl;
+
+            const GLfloat g_vertex_buffer_data[]={
+                -1.0,-1.0,0,
+                1.0,-1.0,0,
+                0.0,1.0,0,
+            };
+            //在显卡中申请内存，内存句柄是vertexbuffer
+            //VAO创建
+            glGenVertexArrays(1,&vao);
+            glBindVertexArray(vao);
+
+            //VBO创建
+            glGenBuffers(1, &vbo);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(
+                    GL_ARRAY_BUFFER,
+                    sizeof(g_vertex_buffer_data),
+                    g_vertex_buffer_data,
+                    GL_STATIC_DRAW);
+
+            glVertexAttribPointer(
+                    vertexPosition,
+                    3,
+                    GL_FLOAT,
+                    GL_FALSE,
+                    0,
+                    (GLvoid*)0);
+        }
+
+        void render(double elapse) override
+        {
+            glClearColor(0,0,0.4,1.0);
+            glClear(GL_COLOR_BUFFER_BIT);
+            //can take out of this
+            glEnableVertexAttribArray(vertexPosition);
+            glBindVertexArray(vao);
+            glUseProgram(this->myProgramId);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexPosition);
+            glBindVertexArray(this->vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glDisableVertexAttribArray(vertexPosition);
+            glfwSwapBuffers(this->mWindow);
+        }
+};
+
+int main(int argc,char **argv)
+{
+    Application app(argc,argv);
+    return app.run();
+}
---
+++ 17_glsl_ebo/shader.vert
@@ -0,0 +1,8 @@
+#version 330
+//属性，这个属性用来获取定点位置，具体的顶点值，由C++传值
+layout(location=0) in vec3 vertexPosition;
+//shader主程序
+void main()
+{
+    gl_Position = vec4 ( vertexPosition,1.0);
+}
