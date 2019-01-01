e75f5094d25478f49a5c3ae25803ba00f072f61c
liangboyan <liangboyanzaz@163.com>
2019-01-01 22:27:31 +0800

    support glew

--- common/MyStudyHeaders.h
+++ common/MyStudyHeaders.h
@@ -1,12 +1,11 @@
 #pragma once
+
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #ifdef __APPLE__
 #   include <GLUT/glut.h>
-#   include <OpenGL/gl.h>
-#   include <OpenGL/glu.h>
-#   define glGenVertexArrays(...) glGenVertexArraysAPPLE(__VA_ARGS__)
-#   define glDeleteVertexArrays(...) glDeleteFencesAPPLE(__VA_ARGS__)
-#   define glBindVertexArray(...) glBindVertexArrayAPPLE(__VA_ARGS__)
-#   define glIsVertexArray(...) glIsVertexArrayAPPLE(__VA_ARGS__)
 #else
 #define GL_GLEXT_PROTOTYPES
 #   include <GL/gl.h>
@@ -14,7 +13,5 @@
 #   include <GL/glu.h>
 #   include <GL/glext.h>
 #endif
-#include <stdio.h>
-#include <GLFW/glfw3.h>
 
 #define MY_LOG_DEBUG(...) do{printf(__VA_ARGS__);fflush(stdout);}while(0)
